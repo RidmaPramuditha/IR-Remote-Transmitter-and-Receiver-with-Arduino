56d1fa055b29a323819576273702b542081f2422
Pramuditha <49787217+Pramu94@users.noreply.github.com>
2022-01-08 16:36:41 +0530

    IR Hex test
    IR Hex test

---
+++ IR_Hex_Test/IR_Hex_Test.ino
@@ -0,0 +1,21 @@
+#include "IRremote.h"
+
+int IRpin = D7;
+IRrecv irrecv(IRpin);
+decode_results results;
+
+void setup()
+{
+Serial.begin(9600);
+irrecv.enableIRIn(); // Start the receiver
+}
+
+void loop()
+{
+if (irrecv.decode(&results))
+{
+Serial.println(results.value, HEX); // Print the Serial 'results.value'
+irrecv.resume(); // Receive the next value
+}
+
+}
